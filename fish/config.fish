911cb075d6941bef337c7e49851540b3b1d9e2bc
Daniel Treacy <dtreacy@atlassian.com>
2018-08-24 15:38:40 -0700

    Recent changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,23 @@
-# emacs ansi-term support
-if test -n "$EMACS"
-  set -x TERM eterm-color
+# fish config
+
+# use vim, i'm not an animal
+set -U EDITOR vim
+
+# load everything in ~/bin
+set PATH $HOME/bin $PATH
+
+# install jenv
+# set PATH $HOME/.jenv/bin $PATH
+jenv init ->/dev/null
+export JAVA_HOME=$HOME/.jenv/versions/(jenv version-name)
+
+function reload
+  source $HOME/.config/fish/config.fish
 end
 
-# this function may be required
-function fish_title
-  true
-end
+bass source $HOME/bin/exports.sh
+
+bass source ~/.nvm/nvm.sh --no-use ';' nvm>/dev/null
+
+# istio
+set PATH $HOME/istio/istio-0.2.12/bin $PATH
